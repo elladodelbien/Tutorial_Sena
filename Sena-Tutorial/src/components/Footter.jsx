2fe0c1bbdf130083dc3d661999a9442405755b5d
Cueva <jhalexander18@hotmail.com>
2023-12-08 16:14:32 -0500

    se trabaja en el archivo index y se ajustan los estilos a mobile

--- Sena-Tutorial/src/components/Footter.jsx
+++ Sena-Tutorial/src/components/Footter.jsx
@@ -3,7 +3,9 @@ import "../App.css";
 const Footter = () => {
   return (
     <div className="contenedor-Footer grid grid-rows-4 grid-cols-1  md:grid-cols-4 md:m-10 md:h-80 h-60 text-white p-4  m-3  text-xs  md:p-12 ">
-      <div className="m-3 p-3"  >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo aperiam modi ducimus voluptatem   </div>
+      <div className="m-3 p-3"  >Lorem ipsum dolor sit, amet br
+       <br />  consectetur adipisicing elit. Nemo aperiam modi ducimus voluptatem <br />
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, eveniet? Animi, qui corrupti. Minima reprehenderit tempore, aliquid nam at maxime unde omnis ipsa ratione   </div>
       <div className="m-3 p-3" >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis explicabo ratione nisi  </div>
       <div className="m-3 p-3" >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis explicabo ratione nisi  </div>
       <div className="m-3 p-3" >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis explicabo ratione nisi  </div>
--- Sena-Tutorial/src/components/Layout.jsx
+++ Sena-Tutorial/src/components/Layout.jsx
@@ -1,11 +1,38 @@
 import { motion, useScroll } from "framer-motion";
-import { useRef } from "react";
+import { useRef, useState,useEffect } from "react";
 import { Outlet, useLocation, Link } from "react-router-dom";
 import Footter from "./Footter";
 
 const Layout = () => {
   const location = useLocation();
+  const [verMenu,setVerMenu] = useState(false)
   const pn = location.pathname;
+  const [isMobile,setIsMobile] = useState(null)
+
+
+  // verificar el dispositivo ancho y alto
+  useEffect(() => {
+    const verificarAnchoPantalla = () => {
+      const anchoPantalla = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+      setIsMobile(anchoPantalla < 550);
+    };
+
+    // Verificar el ancho de la pantalla al cargar la página
+    verificarAnchoPantalla();
+
+    // Agregar un listener para verificar el ancho de la pantalla cuando cambie
+    window.addEventListener('resize', verificarAnchoPantalla);
+
+    // Limpiar el listener cuando el componente se desmonta
+    return () => {
+      window.removeEventListener('resize', verificarAnchoPantalla);
+    };
+  }, [])
+
+
+  const handleMenu =()=>{
+    setVerMenu(!verMenu)
+  }
   console.log(location);
 
   return (
@@ -15,16 +42,18 @@ const Layout = () => {
       <div className=" h-auto  first-letter: w-screen ">
         <header
           id=""
-          className="z-40 w-screen  text-white   h-14 md:flex md:justify-between items-center p-6"
+          className="z-40 w-screen  text-white    h-14 md:flex md:justify-between items-center p-1 md:p-6"
         >
-          <div className=" p-7 text-4xl">
-            <p>Logo</p>
+          <div className=" md:p-7 text-2xl">
+            <p className="md:block hidden">Logo</p>
+            <button onClick={handleMenu} className="md:hidden block">Menu</button>
           </div>
-          <nav className="hidden md:block ">
-            <ul className="text-2xl">
+          <nav className="translate-x-0    md:block ">
+            {/* <ul className="md:text-2xl text-xl md:block md:relative absolute   bg-slate-500  "> */}
+            <ul className={`md:text-2xl text-xl md:block ${isMobile && !verMenu ? "Ocultarmenu" : ""}     md:relative absolute  ${verMenu ? "flex  menu   flex-col" : "  flex  flex-col  "}  bg-slate-500  `}>
               <Link
-                to={"/"}
-                className={`${pn === "/" ? "text-red-500" : "text-white"}`}
+                to={"/"} 
+                className={`${pn === "/" ? "text-red-500" : "text-white"} m-5`}
               >
                 Home
               </Link>
@@ -36,7 +65,7 @@ const Layout = () => {
           </nav>
         </header>
 
-        {/* contenedor de cada una de las paginas */}
+        {/* contenedor de cada una de las páginas */}
         <div className="flex-grow  flex flex-col justify-between">
           <main className="flex-grow  w-full md:flex flex-wrap justify-center m-1  ">
             <Outlet />
