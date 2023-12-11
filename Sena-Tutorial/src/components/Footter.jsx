7cda06ac70222bf40382d12441c4ae24420a7624
Cueva <jhalexander18@hotmail.com>
2023-12-11 13:28:28 -0500

    se acomoda footer

--- Sena-Tutorial/src/components/Footter.jsx
+++ Sena-Tutorial/src/components/Footter.jsx
@@ -2,13 +2,12 @@ import "../App.css";
 
 const Footter = () => {
   return (
-    <div className="contenedor-Footer grid grid-rows-4 grid-cols-1  md:grid-cols-4 md:m-10 md:h-80 h-60 text-white p-4  m-3  text-xs  md:p-12 ">
-      <div className="m-3 p-3"  >Lorem ipsum dolor sit, amet br
-       <br />  consectetur adipisicing elit. Nemo aperiam modi ducimus voluptatem <br />
-       Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed, eveniet? Animi, qui corrupti. Minima reprehenderit tempore, aliquid nam at maxime unde omnis ipsa ratione   </div>
+    <div className="contenedor-Footer grid   grid-cols-2  md:grid-cols-4 md:m-10 md:h-80 h-60 text-white p-6  m-3  text-xs  md:p-12 ">
+      
       <div className="m-3 p-3" >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis explicabo ratione nisi  </div>
       <div className="m-3 p-3" >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis explicabo ratione nisi  </div>
       <div className="m-3 p-3" >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis explicabo ratione nisi  </div>
+      <div className="m-3 p-3 mb-10" >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis explicabo ratione nisi  </div>
      
     </div>
   );
--- Sena-Tutorial/src/components/Loader.jsx
+++ Sena-Tutorial/src/components/Loader.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 
 const Loader = () => {
   return (
-    <div className="loader">
-      <p>Cargando...</p>
+    <div className="loader absolute text-white mx-40  w-4/5 ">
+      <p className='text-2xl'>Cargando...</p>
       {/* Puedes agregar un ícono o animación aquí */}
     </div>
   );
